4a57638e5cb15c393514ffdee41773cdec25a889
Samir Nagib <samir.nagib@gmail.com>
2022-10-06 20:21:51 -0300

    Pesquisa de dados nos databases concluida

--- qy_cvbill.py
+++ qy_cvbill.py
@@ -2,6 +2,7 @@ from uuid import UUID
 import pyodbc 
 import mysql.connector
 import pandas as pd
+import time
 
 # autenticação no banco
 db_destiny = mysql.connector.connect(
@@ -17,7 +18,10 @@ db_source = pyodbc.connect('Driver={SQL Server};'
                       'Database=commserv;'
                       'Trusted_Connection=no;'
                       'UID=script;'
-                      'PWD=Samir102022')
+                      'PWD=Samir102022;'
+                      'unicode_results=True')
+db_source.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8' )
+db_source.setencoding(encoding='utf-8')
 
 #cria os cursores dos bancos
 c_sqlsrv = db_source.cursor()
@@ -25,12 +29,12 @@ c_mysql = db_destiny.cursor()
 
 #querys de consulta
 # simula a leitura dos dados do relatório recebidos via api
-qy_destiny_sel_rp = "SELECT distinct Client FROM cvbill_prd.rel_charge order by Client limit 10;"
+qy_destiny_sel_rp = "SELECT distinct Client FROM cvbill_prd.rel_charge where Client like 'rj2%' order by Client"# limit 10;"
 #busca os dados na base cvbill os clientes 
 qy_destiny_sel_cn = "SELECT idClient, clientName, UUID FROM cvbill_prd.clientes where clientName like '{}%' order by clientName"
 qy_destiny_sel_id = "SELECT idClient, clientName, UUID FROM cvbill_prd.clientes where UUID='{}' order by clientName"
 qy_destiny_upd = "UPDATE clientes SET UUID='{}' WHERE ( idClient = '{}');"
-qy_source = "SELECT id,name,GUID FROM commserv.[dbo].[APP_Client] WHERE name like '{}%'"
+qy_source = "SELECT id,displayName,GUID FROM commserv.[dbo].[APP_Client] WHERE displayName like '{}%'"
 
 #print(qy_destiny_sel_cn.format(server_valor))
 #c_mysql.execute(qy_destiny_sel_cn.format(server_valor))
@@ -38,20 +42,33 @@ qy_source = "SELECT id,name,GUID FROM commserv.[dbo].[APP_Client] WHERE name lik
 c_mysql.execute(qy_destiny_sel_rp)
 r_mysql = c_mysql.fetchall()
 
-#print("Tamanho resultado da query: ", len(r_mysql))
 
+
+#para cada item na pesquisa do relatório
 for x in r_mysql:
-    print("Executanto a pesquisa do serviror: ", x[0])
-    print(qy_source.format(x[0]))
+    #print("Executanto a pesquisa do serviror: ", x[0])
+    #print(qy_source.format(x[0]))
     #pesquisa o nome do servidor na base do commvault para receber o UUID
     c_sqlsrv.execute(qy_source.format(x[0]))
     r_sqlsrv = c_sqlsrv.fetchone()
     # guarda as informações do servidor e do uuid
     server_valor = r_sqlsrv[1]
     server_uuid = r_sqlsrv[2]
-    print(server_valor)
-    print(server_uuid)
+    #print(server_valor)
+    #print(server_uuid)
+    
+    #pesquisa na base do cvbill o server pelo uudi
+    print(qy_destiny_sel_id.format(server_uuid))
     c_mysql.execute(qy_destiny_sel_id.format(server_uuid))
-    r_mysql2 = c_mysql.fetchall()
-    for server in r_mysql2:
-       pass 
+    r_mysql2 = c_mysql.fetchone()
+    if r_mysql2 != None:
+      #encontra procede com a inserção dos dados
+      for server in r_mysql2:
+        print("Servidor Encontrado")
+        print(r_mysql2)
+        time.sleep(0.5)
+    else:
+      # cadastra o novo servidor
+      print("Cadastrando Novo Servidor")
+      time.sleep(2.5)
+
--- report.py
+++ report.py
@@ -42,7 +42,7 @@ else:
 
 
 
-url_report = "http://cmay22wb01.globoi.com/webconsole/api/cr/reportsplusengine/datasets/METRICS_DEFAULT/data?livefeed=true&parameter.param8=4&parameter.param9=0&offset=0&dateFormat=milliseconds&priority=normal&nullValue=&syscol=false&parameter.param10=&limit=-1&parameter.param2=5&parameter.param3=0&parameter.param1=-1&parameter.param11=&parameter.param6=TB&operation=METRICS_EXECUTE_SP&parameter.param7=NULL&parameter.spName=RptMonthlyStorageUsage&parameter.param4=1&parameter.param5=2022-10-02 00:00:00"
+url_report = "http://cmay22wb01.globoi.com/webconsole/api/cr/reportsplusengine/datasets/METRICS_DEFAULT/data?livefeed=true&parameter.param8=4&parameter.param9=0&offset=0&dateFormat=milliseconds&priority=normal&nullValue=&syscol=false&parameter.param10=&limit=-1&parameter.param2=5&parameter.param3=0&parameter.param1=-1&parameter.param11=&parameter.param6=TB&operation=METRICS_EXECUTE_SP&parameter.param7=NULL&parameter.spName=RptMonthlyStorageUsage&parameter.param4=1&parameter.param5=2022-10-05 00:00:00"
 
 report_headers = { "Accept":"application/json", "AuthToken":senha}
 
@@ -114,18 +114,18 @@ for y in report_data['records']:
     Total_Cost.append(y[22])
     Copy_Type.append(y[23]) 
 
-pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_rows', None)
 #pd.set_option('display.max_cols', None)
-#rel_charge = pd.DataFrame(
-#    list(zip(Billing_Tag,Client,Instance,Backupset,Subclient,Storage_Policy,Copy,Front_End_Backup_Size,Front_End_Archive_Size,Primary_App_Size,Protected_App_Size,Media_Size)),
-#    columns=['Billing Tag','Client','Instance','Backupset','Subclient','Storage_Policy','Copy','FEB Size','FEA Size','Primary AppSize','Protected App Size', 'Media Size']
-#)
+rel_charge = pd.DataFrame(
+    list(zip(Billing_Tag,Client,Instance,Backupset,Subclient,Storage_Policy,Copy,Front_End_Backup_Size,Front_End_Archive_Size,Primary_App_Size,Protected_App_Size,Media_Size)),
+    columns=['Billing Tag','Client','Instance','Backupset','Subclient','Storage_Policy','Copy','FEB Size','FEA Size','Primary AppSize','Protected App Size', 'Media Size']
+)
 
 #VERSÃO REDUZIDA
-rel_charge = pd.DataFrame(
-    list(zip(Client)),
-    columns=['Client']
-)
+#rel_charge = pd.DataFrame(
+#    list(zip(Billing_Tag,Client,Instance)),
+#    columns=['Billing Tag','Client','Instance']
+#)
 
 dataf = datetime.datetime.now()
 arq = "rel_charge_"+dataf.strftime("%Y%m%d_%H%M%S")+".csv"
